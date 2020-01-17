8cd7c1d7da856dd235bee2536b8c42376927e54f
Hedi Ben Ahmed <hedi.bahmed@gmail.com>
2020-01-17 13:51:43 +0100

    [CHORE] Use Gradle

---
+++ src/main/java/com/hediapps/apigateway/filter/SimpleFilter.java
@@ -0,0 +1,32 @@
+package com.hediapps.apigateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleFilter extends ZuulFilter {
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        ctx.addZuulRequestHeader("Test", "TestSample");
+        return null;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FilterConstants.PRE_DECORATION_FILTER_ORDER;
+    }
+}
